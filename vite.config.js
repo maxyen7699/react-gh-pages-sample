78fe66b0b247d0cbc6d7fef5b067fd6b79ff50a4
顏仲陽 <yanzhongyang@yanzhongyangdeMacBook-Air.local>
2023-09-10 18:08:24 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NOD_ENV === 'production' ? 'react-gh-pages-sample' : '/',
+  base: '/react-gh-pages-sample/',
   plugins: [react()],
 })
